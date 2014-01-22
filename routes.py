311e9311aff6b172352e4fd2266d3faa6ba5db0e
josh <josh@fuckbook.home>
2014-01-21 20:21:41 -0500

    added pseudomethod weather_json in routes.py which returns static weather data

--- routes.py
+++ routes.py
@@ -3,6 +3,7 @@
 
 from flask import Flask
 import flask
+import json
 
 app = Flask(__name__)
 app.debug = True
@@ -10,16 +11,19 @@ app.debug = True
 @app.route('/')
 def weather_view():
   return flask.render_template('weather_view.html')
-  
-def parse_weather_json(self, json):
-  return { 'temperature' : -5.5,
+
+@app.route('weather.json')
+def weather_json():
+  # STATIC DATA
+  # CHANGE THIS
+  # JUST FOR TESTING
+  return json.dumps({ 'temperature' : -5.5,
             'relative_humidity' : 85,
             'wind_speed' : 20,
             'wind_direction' : 0.75,
             'rainfall' : 2.4, 
             'barometer' : 101.3,
-            'light' : 0.44 }
-            
+            'light' : 0.44 })
 
 if __name__ == '__main__':
   app.run()
