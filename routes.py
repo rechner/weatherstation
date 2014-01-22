22524c6a79b43ad896f84cb912023eb2775d6393
Zac Sturgeon <jkltechinc@gmail.com>
2014-01-21 20:15:09 -0500

    Added initial json template

--- routes.py
+++ routes.py
@@ -10,6 +10,16 @@ app.debug = True
 @app.route('/')
 def weather_view():
   return flask.render_template('weather_view.html')
+  
+def parse_weather_json(self, json):
+  return { 'temperature' : -5.5,
+            'relative_humidity' : 85,
+            'wind_speed' : 20,
+            'wind_direction' : 0.75,
+            'rainfall' : 2.4, 
+            'barometer' : 101.3,
+            'light' : 0.44 }
+            
 
 if __name__ == '__main__':
   app.run()
